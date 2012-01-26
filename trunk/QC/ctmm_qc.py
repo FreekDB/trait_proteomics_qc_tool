44799277c7b4651e2bb7b712fa591895839b1dbb
marcelkempenaar@gmail.com <marcelkempenaar@gmail.com@b51951ae-cf43-402d-b919-4f4a6abba67d>
2012-01-26 14:51:47 +0000

    Changed paths for use with webserver (change _WEB_DIR accordingly)

--- trunk/QC/ctmm_qc.py
+++ trunk/QC/ctmm_qc.py
@@ -1,11 +1,12 @@
-import sys
-import os
-import re
 from string import Template
 from argparse import ArgumentParser
 from subprocess import Popen, PIPE
+from datetime import date
 #from watchdog.observers import Observer
 #from watchdog.events import LoggingEventHandler
+import sys
+import os
+import re
 
 # Globals
 _RAW_FILE = ''
@@ -31,14 +32,14 @@ _METRICS = {
 	'p_c_ids': ['', 0, re.compile('')]	
 }
 
-# Paths (They should be adapted for the system they run on)
+# Paths (These should be adapted for the system they run on)
 _IN_DIR = ''
 _OUT_DIR = ''
 _COPY_LOG = ''
-_WEB_ROOT = os.path.normpath(r'E:\Web')
-_NIST = os.path.normpath(r'C:\Users\nbic\Documents\NISTMSQCv1_2_0')
-_PROGRES_LOG = 'qc_status.log'
-_QC_HOME = os.path.normpath(r'E:\QC')
+_WEB_DIR = os.path.normpath(r'E:\\Web')
+_NIST = os.path.normpath(r'C:\\Users\\nbic\\Documents\\NISTMSQCv1_2_0')
+_PROGRES_LOG = r'qc_status.log'
+_QC_HOME = os.path.normpath(r'E:\\QC')
 
 def monitor_input():
 	print 'Version 0.0.2'
@@ -48,7 +49,7 @@ def monitor_input():
 	global _RAW_FILE, _BASE_NAME, _OUT_DIR
 	files = _read_logfile()
 
-	# The following code should be threaded, one thread to monitor for new files
+	# The following code should (preferably) be threaded, one thread to monitor for new files
 	# and another thread for each file to be processed
 	files = _parse_robocopy_log(files)
 
@@ -60,14 +61,12 @@ def monitor_input():
 		if status == 'new':
 			print "-----------\nProcessing:\n\t", f, "\n-----------\n"
 			# Set for current file
-			_RAW_FILE = '{0}\\{1}'.format(_IN_DIR, f)
+			_RAW_FILE = r'{0}\\{1}'.format(_IN_DIR, f)
 			_BASE_NAME = os.path.splitext(f)[0]
-			_OUT_DIR = '{0}\\{1}_QC\\'.format(_OUT_DIR, _BASE_NAME)
+			_OUT_DIR = r'{0}\\{1}_QC\\'.format(_OUT_DIR, _BASE_NAME)
 
-			# Create _OUT_DIR storing NIST output as well as graphics etc.
-			d = os.path.dirname(_OUT_DIR)
-			if not os.path.exists(d):
-				os.makedirs(d)
+			# Create folders for storing temp output as well as web output
+			_manage_paths()
 			
 			files[f] = 'processing'
 			
@@ -104,6 +103,34 @@ def _log_progress():
 	simple status report through a webserver."""
 	pass
 
+def _manage_paths():
+	# Create directory on webserver part for storing images and the report
+	today = date.today()
+	y = os.path.normpath(r'{0}\\{1}'.format(_WEB_ROOT, today.year))
+	m = os.path.normpath(r'{0}\\{1}'.format(today.year, today.month))
+	d = os.path.normpath(r'{0}\\{1}'.format(today.month, today.day))
+	web = os.path.normpath(r'{0}\\{1}'.format(today.day, _BASE_NAME))
+	
+	if not os.path.exists(y):
+		os.makedirs(y)
+	if not os.path.exists(m):
+		os.makedirs(m)
+	if not os.path.exists(d):
+		os.makedirs(d)
+	if not os.path.exists(web):
+		os.makedirs(web)
+	# TODO: else, folder exists, something went wrong?
+
+	global _WEB_DIR
+	_WEB_DIR = web
+	
+	# Create _OUT_DIR storing NIST output
+	project = os.path.dirname(_OUT_DIR)
+	if not os.path.exists(project):
+		os.makedirs(project)
+	# TODO: else, folder exists, something went wrong?
+		
+
 def _run_NIST():
 	"""Starts the NIST metrics workflow using a RAW file as input. Reads parameters
 	from a configuration file to pass as arguments to the workflow."""
@@ -117,8 +144,8 @@ def _run_NIST():
 	
 	# Run NIST pipeline
 	# TODO: validate parameters, check if in- and out-dir can be the same
-	NIST_exe = 'perl {0}\\scripts\\run_NISTMSQC_pipeline.pl'.format(_NIST)
-	NIST_cmd = '{0} --in_dir {1} --out_dir {2} --library {3} --instrument_type {4} {5} {6} {7} {8}'.format(NIST_exe,
+	NIST_exe = r'perl {0}\\scripts\\run_NISTMSQC_pipeline.pl'.format(_NIST)
+	NIST_cmd = r'{0} --in_dir {1} --out_dir {2} --library {3} --instrument_type {4} {5} {6} {7} {8}'.format(NIST_exe,
 				_IN_DIR, _OUT_DIR, nist_library, instrument, '--overwrite_searches', '--pro_ms', '--log_file', '--mode lite')
 	#sys.exit("NIST_cmd: \n----------\n\n{0}\n\n".format(NIST_cmd))
 	
@@ -132,13 +159,12 @@ def _run_R_script():
 	"""After running the NIST metrics workflow, the mzXML file created can be read in R
 	and processed further (graphics and basic metrics)"""
 	# Execute Rscript (arguments: input mzXML, output PDF prefix, MSlevel)
-	Rcmd = 'Rscript {0} {1} {2} {3}'.format(_R_GRAPHICS, '{0}\\{1}'.format(_OUT_DIR, _BASE_NAME), 
-											'{0}\\images\\{1}'.format(_WEB_ROOT, _BASE_NAME), 1)
+	Rcmd = 'Rscript {0} {1} {2} {3}'.format(_R_GRAPHICS, _RAW_FILE, r'{0}\\{1}'.format(_WEB_DIR, _BASE_NAME), 1)
 	_run_command(Rcmd)
 	
 def _create_metrics():
 	""" Parses NIST metrics output file extracting relevant metrics subset """
-	metrics_file = os.path.normpath('{0}\\{1}_report.msqc'.format(_OUT_DIR, _BASE_NAME))
+	metrics_file = os.path.normpath(r'{0}\\{1}_report.msqc'.format(_OUT_DIR, _BASE_NAME))
 	metrics_log_file = os.path.normpath('{0}.LOG'.format(metrics_file))
 	print "\n### Metrics File: ", metrics_file
 
@@ -149,23 +175,23 @@ def _create_metrics():
 	for metric in _METRICS.keys():
 		index = next((num for num, line in enumerate(nist_metrics) if _METRICS[metric][0] in line), None)
 		if index != None:
-			print index
 			result = _METRICS[metric][-1].search(nist_metrics[index + _METRICS[metric][1]])
 			_METRICS[metric] = result.group(1)
 			
 	# Extracting metrics (MS1, MS2 scans) from NIST log file
-	with open(metrics_log_file, 'f') as f:
+	with open(metrics_log_file, 'r') as f:
 		nist_metrics_log = f.readlines()
 		
-	_METRICS['ms1_spectra'] = re.search('([0-9]+) ms1 spectra').group(1)
-	_METRICS['ms2_spectra'] = re.search('([0-9]+) ms2 spectra').group(1)
+	# TODO: error handling
+	_METRICS['ms1_spectra'] = re.search('([0-9]+) ms1 spectra', nist_metrics_log).group(1)
+	_METRICS['ms2_spectra'] = re.search('([0-9]+) ms2 spectra', nist_metrics_log).group(1)
 	
 	# Other generic metrics
 	_METRICS['f_size'] = "%0.1f" % (os.stat(_RAW_FILE).st_size/(1024*1024.0))
 
 def _create_report():
 	# Graphics to include
-	heatmap = '{0}_heatmap.pdf'.format(_BASE_NAME)
+	heatmap = r'{0}\\{1}_heatmap.pdf'.format(_WEB_DIR, _BASE_NAME)
 	ion_cnt = '{0}_ions.pdf'.format(_BASE_NAME)
 
 	# Place values and graphics in template HTML file
@@ -173,26 +199,27 @@ def _create_report():
 		template = f.readlines()
 	
 	report_template = Template(template)
-	report_updated = report_template.safe_substitute(# General
-													raw_file=_RAW_FILE,
-													date='',
-													time='',
-													runtime='',
-													# Metrics
-													m_fs=_METRICS['f_size'],
-													m_ms1_scans=_METRICS['ms1_spectra'],
-													m_ms2_scans=_METRICS['ms2_spectra'],
-													m_f_ms1_rt=_METRICS['f_ms1_rt'],
-													m_l_ms1_rt=_METRICS['l_ms1_rt'],
-													m_m_p_w=_METRICS['m_p_w'],
-													m_i_i_t_ms1=_METRICS['i_i_t_ms1'],
-													m_i_i_t_ms2=_METRICS['i_i_t_ms2'],
-													m_p_c_pep=_METRICS['p_c_pep'],
-													m_p_c_ion=_METRICS['p_c_ion'],
-													m_p_c_ids=_METRICS['p_c_ids'],
-													# Figures
-													heatmap_img=heatmap,
-													ions_img=ion_cnt)
+	report_updated = report_template.safe_substitute(
+		# General
+		raw_file=_RAW_FILE,
+		date='',
+		time='',
+		runtime='',
+		# Metrics
+		m_fs=_METRICS['f_size'],
+		m_ms1_scans=_METRICS['ms1_spectra'],
+		m_ms2_scans=_METRICS['ms2_spectra'],
+		m_f_ms1_rt=_METRICS['f_ms1_rt'],
+		m_l_ms1_rt=_METRICS['l_ms1_rt'],
+		m_m_p_w=_METRICS['m_p_w'],
+		m_i_i_t_ms1=_METRICS['i_i_t_ms1'],
+		m_i_i_t_ms2=_METRICS['i_i_t_ms2'],
+		m_p_c_pep=_METRICS['p_c_pep'],
+		m_p_c_ion=_METRICS['p_c_ion'],
+		m_p_c_ids=_METRICS['p_c_ids'],
+		# Figures
+		heatmap_img=heatmap,
+		ions_img=ion_cnt)
 	
 	# Write report file to directory holding all reports
 	with open('{0}/{1}_report.html'.format(_WEB_ROOT, _BASE_NAME), 'w') as f:
@@ -225,8 +252,8 @@ def _parse_robocopy_log(files):
 def _run_msconvert():
 	msconvert = '{0}\\bin\\2562\\msconvert.exe'.format(_NIST)
 	# Both mzXML and MGF files need to be created
-	mzXML_cmd = '{0} {1} -o {2} --mzXML -e .RAW.mzXML'.format(msconvert, _RAW_FILE, _OUT_DIR)
-	MGF_cmd   = '{0} {1} -o {2} --mgf -e .RAW.MGF'.format(msconvert, _RAW_FILE, _OUT_DIR)
+	mzXML_cmd = r'{0} {1} -o {2} --mzXML -e .RAW.mzXML'.format(msconvert, _RAW_FILE, _OUT_DIR)
+	MGF_cmd   = r'{0} {1} -o {2} --mgf -e .RAW.MGF'.format(msconvert, _RAW_FILE, _OUT_DIR)
 
 	# Execute msconvert for both output files
 	print "\tConverting to mzXML.."
@@ -252,6 +279,9 @@ def _run_command(cmd):
 	result, err = run.communicate()
 	if run.returncode != 0:
 		raise IOError(err)
+
+def _cleanup():
+	pass
 
 def main(args):
 	global _IN_DIR, _OUT_DIR, _COPY_LOG
