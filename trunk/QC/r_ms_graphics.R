13c3d3c3aebfb3d50f05935c9903338b0ebaa43b
marcelkempenaar@gmail.com <marcelkempenaar@gmail.com@b51951ae-cf43-402d-b919-4f4a6abba67d>
2012-02-20 12:07:14 +0000

    Changed x-axis to relfect Retention Times (instead of scan number), added PDF to output

--- trunk/QC/r_ms_graphics.R
+++ trunk/QC/r_ms_graphics.R
@@ -1,33 +1,44 @@
 library(fields)
 library(readMzXmlData)
 
-ms_image <- function(mzXML, PDF, bins=100, min.mz=300, max.mz=2000, mslevel=1, peaks=100, method="max") {	
+ms_image <- function(mzXML, pfile=FALSE, min.rt=NULL, max.rt=NULL, n.bins=100, mslevel=1, n.peaks=100, method="max") {	
+	## Creates a heatmap-like image of all scans from the experiment
+	## Bins the data into n.bins bins and shows either the 'max' or 'average' of all bins
 	
-	window = (max.mz - min.mz) / bins
+	if (is.null(min.rt))
+	    min.rt = mzXML[[1]]$metaData$retentionTime
+	if (is.null(max.rt))
+	    max.rt = mzXML[[length(mzXML)]]$metaData$retentionTime
+	    
+	window = (max.rt - min.rt) / n.bins
 	s = c()
-	for ( i in 1:((max.mz - min.mz) / window))
-		s[i] = min.mz + window * (i-1)
+	for ( i in 1:((max.rt - min.rt) / window))
+		s[i] = min.rt + window * (i-1)
 
     logger("Creating heatmap image..")
 	m = matrix(ncol=length(s))
 	r = c()
-	n = c()
+	rt = c()
 	for (scan in 1:length(mzXML)) {
-		if (mzXML[[scan]]$metaData$peaksCount > 500 & 
+	    ## Get data for all scans for the given ms level and with a minimum number of 'n.peaks' peaks. 
+		if (mzXML[[scan]]$metaData$peaksCount > n.peaks & 
 			mzXML[[scan]]$metaData$msLevel == mslevel) {
-			n = c(n, scan)
-			r = bin.scan(mzXML[[scan]]$spectrum, s, window, min.mz, max.mz, method)
+			r = bin.scan(mzXML[[scan]]$spectrum, s, window, min.rt, max.rt, method)
 			m = rbind(m, r)
+			## Register all retention times used for x-axis
+			rt = c(rt, mzXML[[scan]]$metaData$retentionTime)
 		}
 	}
 	m = m[-1,]
 	
 	logger(paste("Done preparing data..", sep=""))
-	
-    bin.plot(m, s, n, PDF)
+	print("Creating image")
+    bin.plot(m, s, rt, pfile)
 }
 
-bin.scan <- function(scan, bins, window, min.mz, max.mz, method) {
+bin.scan <- function(scan, bins, window, min.rt, max.rt, method) {
+    ## Combines all data from a bin given its left and right margin parameters
+    ## using either the 'max' or 'average' method.
 	val = NULL
 	mz = scan$mass
 	int = scan$intensity
@@ -48,48 +59,57 @@ bin.scan <- function(scan, bins, window, min.mz, max.mz, method) {
 	return(val)
 }
 
-bin.plot <- function(m, s, n, PDF=FALSE) {
-	## Create a grayscale, NOTE: unused
-	low <- col2rgb("black")/255
-	high <- col2rgb("white")/255
-	r <- seq(low[1], high[1], len=50)
-	g <- seq(low[2], high[2], len=50)
-	b <- seq(low[3], high[3], len=50)
-	pallette <- rgb(r, g, b)
-
-	if (PDF != FALSE)
-		png(paste(PDF, '_heatmap.png', sep=''), width = 800, height = 800)
-	
+bin.plot <- function(m, s, rt, pfile) {
 	## Change margins
 	par(mar=c(4, 4, 2, 1))
-	## Create image with legend
-	image.plot(n, s, log(m), xlab="Scan number (RT)", ylab="mass", main="Spectra overview")
-	
-	if (PDF != FALSE)
-		dev.off()
-		
-	logger(paste("Done creating heatmap, saved as ", PDF, "_heatmap.png", sep=""))
+
+	if (pfile != FALSE) {
+	    ## Save image to both PDF and PNG files
+		png(paste(pfile, '_heatmap.png', sep=''), width = 800, height = 800)
+    	image.plot(rt, s, log(m), xlab="Retention Time", ylab="mass", main="Spectra overview")
+        dev.off()
+        
+        pdf(paste(pfile, '_heatmap.pdf', sep=''))
+    	image.plot(rt, s, log(m), xlab="Retention Time", ylab="mass", main="Spectra overview")
+    	dev.off()
+	} else {
+	    image.plot(rt, s, log(m), xlab="Retention Time", ylab="mass", main="Spectra overview")
+	}
+	logger(paste("Done creating heatmap, saved as ", pfile, "_heatmap.png", sep=""))
 }
 
-ion_count <- function(mzXML, PDF, mslevel) {
+ion_count <- function(mzXML, pfile=FALSE, mslevel=1) {
+    ## The 'total Ion count' plot shows the sum of the intensities for each 
+    ## complete scan with the RT on the X-axis
     logger("Creating total Ion count graph")
 	ions = c()
+	rt = c()
 	for (scan in 1:length(mzXML)) {
 		# Only plot scans with at least one peak
 		if (mzXML[[scan]]$metaData$peaksCount > 1 & 
-			mzXML[[scan]]$metaData$msLevel == mslevel)
+			mzXML[[scan]]$metaData$msLevel == mslevel) {
 			ions = c(ions, sum(mzXML[[scan]]$spectrum$intensity))
+			rt = c(rt, mzXML[[scan]]$metaData$retentionTime)
+        }
 	}
 	
-	if (PDF != FALSE)
-		png(paste(PDF, '_ions.png', sep=''), width = 800, height = 400)
 	## Change margins
 	par(mar=c(4,4,2,2))
+	
+	if (pfile != FALSE) {
+	    ## Save image to both PDF and PNG files
+	    png(paste(pfile, '_ions.png', sep=''), width = 800, height = 400)
+	    plot(ions, type='l', xlab="Scan number (RT)", ylab="Total Ion Count", main="Ion count per scan")
+	    dev.off()
+
+	    pdf(paste(pfile, '_ions.pdf', sep=''))
+	    plot(ions, type='l', xlab="Scan number (RT)", ylab="Total Ion Count", main="Ion count per scan")
+	    dev.off()
+	}
+	
 	plot(ions, type='l', xlab="Scan number (RT)", ylab="Total Ion Count", main="Ion count per scan")
 	
-	if (PDF != FALSE)
-		dev.off()
-	logger(paste("Done creating total Ion count graph, saved as ", PDF, "_ions.png", sep=""))
+	logger(paste("Done creating total Ion count graph, saved as ", pfile, "_ions.png", sep=""))
 }
 
 read_mzXML <- function(mzXML) {
@@ -115,6 +135,7 @@ ms_metrics <- function(mzXML) {
 }
 
 logger <- function(logdata) {
+    ## Logs progress, adds a timestamp for each event
     process <<- c(process, paste(Sys.time(), "\t", logdata, sep=""))
 }
 
@@ -122,7 +143,7 @@ logger <- function(logdata) {
 args  = commandArgs(TRUE)
 
 mzXML   = args[1] # Input mzXML filename
-o_PDF   = args[2] # PDF path prefix
+o_file  = args[2] # PNG/PDF path prefix
 msl     = args[3] # MS level
 logfile = args[4] # Log file for writing status and some metrics
 process = c()
@@ -131,10 +152,10 @@ data = read_mzXML(mzXML)
 
 ## Creating heatmap of all data
 cat("\t\tCreating heatmap image..\n")
-ms_image(data, o_PDF, mslevel=msl)
+ms_image(data, o_file, mslevel=msl)
 ## Creating total ion count plot of all data
 cat("\t\tCreating total Ion count plot..\n")
-ion_count(data, o_PDF, mslevel=msl) 
+#ion_count(data, o_file, mslevel=msl) 
 ## Generating metrics
 cat("\t\tGenerating metrics..\n")
 ms_metrics(data)
