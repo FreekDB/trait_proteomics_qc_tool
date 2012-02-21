3b50d46f9d9c2c4fc89e2028bf0861545007dadd
marcelkempenaar@gmail.com <marcelkempenaar@gmail.com@b51951ae-cf43-402d-b919-4f4a6abba67d>
2012-02-21 11:12:30 +0000

    Added assertions to existing tests, removed 'day' from path created for report

--- trunk/QC/test/ctmm_qc_test.py
+++ trunk/QC/test/ctmm_qc_test.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import unittest
+from os.path import normpath
 from QC import ctmm_qc
 
 class Test(unittest.TestCase):
@@ -12,9 +13,9 @@ class Test(unittest.TestCase):
 
     def test_logfiles(self):
         files = ctmm_qc._read_logfile(self.logfile)
-        self.assertEquals(files, {'': '', '': ''})
+        self.assertEquals(files, {'110215_13.RAW': 'completed', '110308_02.RAW': 'completed'})
         files = ctmm_qc._parse_robocopy_log(self.robocopylog, files)
-        self.assertEquals(files, {'': '', '': ''})
+        self.assertEquals(files, {'110215_13.RAW': 'completed', 'U87_10mg_B.raw': 'new', '110308_02.RAW': 'completed'})
         
 if __name__ == '__main__':
     unittest.main()
