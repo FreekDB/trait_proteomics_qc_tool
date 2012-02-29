ff857c26cec392782b7eefa04681126b5d7f3011
tim.te.beek@nbic.nl <tim.te.beek@nbic.nl@b51951ae-cf43-402d-b919-4f4a6abba67d>
2012-02-29 16:55:50 +0000

    append || exit 0 to pylint command

--- trunk/make.bat
+++ trunk/make.bat
@@ -1,4 +1,4 @@
 pep8 QC > pep8.txt
 
 nosetests --with-xunit
-pylint --max-line-length=120 -f parseable --include-ids=y QC > pylint.txt
+pylint --max-line-length=120 -f parseable --include-ids=y QC > pylint.txt || exit 0
