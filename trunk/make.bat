797ac3c73f0e7d049e9e46a8b7e0702c7af5c5eb
marcelkempenaar@gmail.com <marcelkempenaar@gmail.com@b51951ae-cf43-402d-b919-4f4a6abba67d>
2012-02-29 13:06:51 +0000

    Changed order, removed exiting code

--- trunk/make.bat
+++ trunk/make.bat
@@ -1,5 +1,4 @@
-pylint --max-line-length=120 -f parseable --include-ids=y QC > pylint.txt || exit 0
-
-pep8 QC > pep8.txt || exit 0
+pep8 QC > pep8.txt
 
 nosetests --with-xunit
+pylint --max-line-length=120 -f parseable --include-ids=y QC > pylint.txt
