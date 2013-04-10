3425a513105a654cc6e222e2460e4ea40c86d48b
pravin.pawar@nbic.nl <pravin.pawar@nbic.nl@b51951ae-cf43-402d-b919-4f4a6abba67d>
2013-04-10 12:12:59 +0000

    Modified unit test for method Reader.prepareReportArrayList();

--- ProteomicsQCReportViewer/test/nl/ctmm/trait/proteomics/qcviewer/zzz_old/input/ReaderTest.java
+++ ProteomicsQCReportViewer/test/nl/ctmm/trait/proteomics/qcviewer/zzz_old/input/ReaderTest.java
@@ -19,18 +19,18 @@ public class ReaderTest {
     public void testPrepareReportListActualData() {
         final Reader reader = new Reader("QCReports/ctmm");
         final List reports = reader.prepareReportArrayList();
-        assertEquals("There should be 44 report units.", 44, reports.size());
+        assertEquals("There should be 213 report units.", 213, reports.size());
         assertEquals(ReportUnit.class, reports.get(5).getClass());
         // Examine one report unit in detail.
         final ReportUnit reportUnit6 = (ReportUnit) reports.get(5);
         assertEquals(6, reportUnit6.getReportNum());
-        assertEquals("1154.9", reportUnit6.getFileSizeString());
-        assertEquals("10944 (10944)", reportUnit6.getMs1Spectra());
-        assertEquals("21306 (21306)", reportUnit6.getMs2Spectra());
-        assertEquals("2012/Dec/04 - 13:58", reportUnit6.getMeasured());
-        assertEquals("0:18:05", reportUnit6.getRuntime());
-        assertEquals("QE1_121005_OPL0000_jurkat2ug_01_heatmap.png", reportUnit6.getHeatmapName());
-        assertEquals("QE1_121005_OPL0000_jurkat2ug_01_ions.png", reportUnit6.getIoncountName());
+        assertEquals("1120.4", reportUnit6.getFileSizeString());
+        assertEquals("5678 (5678)", reportUnit6.getMs1Spectra());
+        assertEquals("30799 (30799)", reportUnit6.getMs2Spectra());
+        assertEquals("2013/Feb/02 - 23:12", reportUnit6.getMeasured());
+        assertEquals("0:24:23", reportUnit6.getRuntime());
+        assertEquals("QE1_121127_OPL1016_DC_Mefopa_CSF_AA1_heatmap.png", reportUnit6.getHeatmapName());
+        assertEquals("QE1_121127_OPL1016_DC_Mefopa_CSF_AA1_ions.png", reportUnit6.getIoncountName());
     }
 
     /**
