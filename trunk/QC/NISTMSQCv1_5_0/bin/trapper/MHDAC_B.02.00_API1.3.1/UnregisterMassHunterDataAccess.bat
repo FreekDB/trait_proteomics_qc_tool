4c705698dc0a01ca81ecea36aa8a3c45c9a54dbb
pawarp <pawarp@b51951ae-cf43-402d-b919-4f4a6abba67d>
2013-06-21 14:11:33 +0000

    Removed NIST components. Renamed trunk to ProteomicsQCPipeline.

--- trunk/QC/NISTMSQCv1_5_0/bin/trapper/MHDAC_B.02.00_API1.3.1/UnregisterMassHunterDataAccess.bat
+++
@@ -1,5 +0,0 @@
-set REGASMPROG=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm.exe
-
-%REGASMPROG% BaseCommon.dll /tlb /u
-%REGASMPROG% BaseDataAccess.dll /tlb /u
-%REGASMPROG% MassSpecDataReader.dll /tlb /u
