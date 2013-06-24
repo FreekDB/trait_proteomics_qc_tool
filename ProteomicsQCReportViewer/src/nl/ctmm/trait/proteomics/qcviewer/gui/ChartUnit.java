5d42aabf0cfc7646fbce715864dac3707e3bbac8
Freek de Bruijn <freekdb@gmail.com>
2013-06-24 23:19:08 +0200

    Fixed most of the unit tests in ChartUnitTest by using new test data files (with some refactoring). The testSetGraphDataSeries test is currently failing/ignored and still needs to be fixed. Replaced tabs by spaces. Small textual changes.

--- ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ChartUnit.java
+++ ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ChartUnit.java
@@ -6,7 +6,6 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.List;
-
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.labels.StandardXYToolTipGenerator;
@@ -23,89 +22,89 @@ import org.jfree.data.xy.XYSeriesCollection;
  * @author <a href="mailto:pravin.pawar@nbic.nl">Pravin Pawar</a>
  * @author <a href="mailto:freek.de.bruijn@nbic.nl">Freek de Bruijn</a>
  */
-
 public class ChartUnit {
-	private String chartName = "";
-	private XYSeries graphDataSeries = null;
-	private int reportNum; 
-	private JFreeChart ticChart = null; 
-	private NumberAxis domainAxis = null;
+    private String chartName = "";
+    private XYSeries graphDataSeries = null;
+    private int reportNum; 
+    private JFreeChart ticChart = null; 
+    private NumberAxis domainAxis = null;
     private NumberAxis rangeAxis = null;
     private XYBarRenderer renderer = null;
-	private XYPlot plot = null;
-	private double maxIntensity = 0; 
-	private String maxIntensityString = "N/A";
+    private XYPlot plot = null;
+    private double maxIntensity;
+    private String maxIntensityString = "N/A";
     private static final List<Color> GRAPH_COLORS = Arrays.asList(
            Color.BLUE, Color.DARK_GRAY, Color.GRAY, Color.MAGENTA, Color.ORANGE, 
            Color.PINK, Color.LIGHT_GRAY, Color.RED, Color.GREEN);
     
-	public ChartUnit(final String msrunName, final int reportNum, final XYSeries series) {
-		this.chartName = msrunName; 
-		this.reportNum = reportNum;
-		this.graphDataSeries = series;
-		
-		if (series != null) {
-			maxIntensity = series.getMaxY();
-			NumberFormat formatter = new DecimalFormat("0.0000E0");
-			maxIntensityString = formatter.format(maxIntensity);
-		}
-		domainAxis = new NumberAxis(null);
-	    rangeAxis = new NumberAxis(null);
-	    renderer = new XYBarRenderer();
-	    //Sets the percentage amount by which the bars are trimmed
-	    renderer.setMargin(0.98); //Default renderer margin is 0.0
-	    renderer.setDrawBarOutline(false); 
-	    renderer.setShadowVisible(false);
-	    Color currentColor = GRAPH_COLORS.get(reportNum%GRAPH_COLORS.size());
-	    renderer.setSeriesPaint(0, currentColor);
-	    renderer.setGradientPaintTransformer(null);
-	    renderer.setSeriesOutlinePaint(0, currentColor);
-	    renderer.setFillPaint(currentColor);
-	    renderer.setPaint(currentColor);
-	    renderer.setBarPainter(new StandardXYBarPainter());
-	    renderer.setSeriesStroke(0, null);
-	    renderer.setBasePaint(Color.WHITE);
-	    XYBarRenderer.setDefaultShadowsVisible(false);
-	    //Add support for toolTip
-	    StandardXYToolTipGenerator toolTipGenerator = new StandardXYToolTipGenerator();
-	    renderer.setBaseToolTipGenerator(toolTipGenerator);
-	    renderer.setDrawBarOutline(false);
-		XYSeriesCollection xyDataset = new XYSeriesCollection(series);
-		//Prepare chart using plot - this is the best option to control domain and range axes
-	    plot = new XYPlot(xyDataset, domainAxis, rangeAxis, renderer);
-	    rangeAxis.setNumberFormatOverride(new DecimalFormat("0E00"));
-	    int style = Font.BOLD;
-	    Font font = new Font ("Garamond", style , 13);
-	    ticChart = new JFreeChart(msrunName + "     MaxIntensity = " + maxIntensityString, font, plot, false);
-	    // performance
-	    ticChart.setAntiAlias(false);
-	}
-	
-	/**
-	 * Get max intensity of TIC graph
-	 * @return Max intensity of TIC graph
-	 */
-	public double getMaxTicIntensity() {
-		if (graphDataSeries == null) {
-			return 0;
-		} else return maxIntensity;
-	}
-	
-	
-	/**
-	 * Get max intensity of TIC graph in String format
-	 * @return Max intensity of TIC graph in String format
-	 */
-	public String getMaxTicIntensityString() {
-		return maxIntensityString;
-	}
-	
+    public ChartUnit(final String msrunName, final int reportNum, final XYSeries series) {
+        this.chartName = msrunName; 
+        this.reportNum = reportNum;
+        this.graphDataSeries = series;
+        
+        if (series != null) {
+            maxIntensity = series.getMaxY();
+            NumberFormat formatter = new DecimalFormat("0.0000E0");
+            maxIntensityString = formatter.format(maxIntensity);
+        }
+        domainAxis = new NumberAxis(null);
+        rangeAxis = new NumberAxis(null);
+        renderer = new XYBarRenderer();
+        //Sets the percentage amount by which the bars are trimmed
+        renderer.setMargin(0.98); //Default renderer margin is 0.0
+        renderer.setDrawBarOutline(false); 
+        renderer.setShadowVisible(false);
+        Color currentColor = GRAPH_COLORS.get(reportNum%GRAPH_COLORS.size());
+        renderer.setSeriesPaint(0, currentColor);
+        renderer.setGradientPaintTransformer(null);
+        renderer.setSeriesOutlinePaint(0, currentColor);
+        renderer.setFillPaint(currentColor);
+        renderer.setPaint(currentColor);
+        renderer.setBarPainter(new StandardXYBarPainter());
+        renderer.setSeriesStroke(0, null);
+        renderer.setBasePaint(Color.WHITE);
+        XYBarRenderer.setDefaultShadowsVisible(false);
+        //Add support for toolTip
+        StandardXYToolTipGenerator toolTipGenerator = new StandardXYToolTipGenerator();
+        renderer.setBaseToolTipGenerator(toolTipGenerator);
+        renderer.setDrawBarOutline(false);
+        XYSeriesCollection xyDataset = new XYSeriesCollection(series);
+        //Prepare chart using plot - this is the best option to control domain and range axes
+        plot = new XYPlot(xyDataset, domainAxis, rangeAxis, renderer);
+        rangeAxis.setNumberFormatOverride(new DecimalFormat("0E00"));
+        int style = Font.BOLD;
+        Font font = new Font ("Garamond", style , 13);
+        ticChart = new JFreeChart(msrunName + "     MaxIntensity = " + maxIntensityString, font, plot, false);
+        // performance
+        ticChart.setAntiAlias(false);
+    }
+    
+    /**
+     * Get max intensity of TIC graph
+     * @return Max intensity of TIC graph
+     */
+    public double getMaxTicIntensity() {
+        // TODO: simply return maxIntensity, since it is 0 when graphDataSeries is null? [Freek]
+        if (graphDataSeries == null) {
+            return 0;
+        } else
+            return maxIntensity;
+    }
+
+    /**
+     * Get max intensity of TIC graph in String format
+     * @return Max intensity of TIC graph in String format
+     */
+    public String getMaxTicIntensityString() {
+        return maxIntensityString;
+    }
+    
     /**
      * Set the value of parameter graphDataSeries
      * @param series
      */
     public void setGraphDataSeries(XYSeries series) {
-    	this.graphDataSeries = series;
+        this.graphDataSeries = series;
     }
     
     /**
@@ -113,7 +112,7 @@ public class ChartUnit {
      * @return ticChart
      */
     public JFreeChart getTicChart() {
-    	return ticChart;
+        return ticChart;
     }
     
     /**
@@ -121,6 +120,6 @@ public class ChartUnit {
      * @return domainAxis
      */
     public NumberAxis getDomainAxis() {
-    	return domainAxis;
+        return domainAxis;
     }
 }
--- ProteomicsQCReportViewer/test/nl/ctmm/trait/proteomics/qcviewer/gui/ChartUnitTest.java
+++ ProteomicsQCReportViewer/test/nl/ctmm/trait/proteomics/qcviewer/gui/ChartUnitTest.java
@@ -1,82 +1,82 @@
 package nl.ctmm.trait.proteomics.qcviewer.gui;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.data.xy.XYSeries;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * Unit tests for the <code>ReportUnit</code> class.
+ * Unit tests for the <code>ChartUnit</code> class.
  *
+ * @author <a href="mailto:pravin.pawar@nbic.nl">Pravin Pawar</a>
  * @author <a href="mailto:freek.de.bruijn@nbic.nl">Freek de Bruijn</a>
  */
 public class ChartUnitTest {
-    private static final int REPORT_NUMBER = 6;
+    /**
+     * Base directory for the TIC files.
+     */
+    private static final String TIC_DIRECTORY = "QCReports\\2013\\Jun\\";
 
     private ChartUnit chartUnit1, chartUnit2, chartUnit3;
-    private XYSeries series1, series2; 
+    private XYSeries xySeries;
 
     /**
-     * Initialize a <code>ChartUnit</code>.
+     * Initialize an <code>XYSeries</code> object and three <code>ChartUnit</code> objects.
      */
     @Before
     public void setUp() {
-    	/*
-    	 * public ChartUnit(final String msrunName, final int reportNum, final XYSeries series)
-    	 */
-    	String msrunName1 = "msrun1";
-    	String msrunName2 = "msrun2";
-    	String msrunName3 = "msrun3";
-    	String ticMatrixFileName1 = "QCReports\\ctmm\\2013\\Feb\\QE1_121127_OPL1016_DC_Mefopa_CSF_AA3\\QE1_121127_OPL1016_DC_Mefopa_CSF_AA3 _ticmatrix.csv";
-    	File ticMatrixFile1 = new File(ticMatrixFileName1);
-    	series1 = readXYSeries(msrunName1, ticMatrixFile1);
-    	String ticMatrixFileName2 = "QCReports\\ctmm\\2013\\Feb\\QE2_121206_OPL1016_DC_Mefopa_CSF_AF4\\QE2_121206_OPL1016_DC_Mefopa_CSF_AF4 _ticmatrix.csv";
-    	File ticMatrixFile2 = new File(ticMatrixFileName2);
-    	series2 = readXYSeries(msrunName1, ticMatrixFile2);
-    	chartUnit1 = new ChartUnit(msrunName1, 1, series1);
-    	chartUnit2 = new ChartUnit(msrunName2, 2, null); //initialize with empty series
-    	chartUnit3 = new ChartUnit(msrunName3, 3, series2);
+        final String msrunName1 = "msrun1";
+        final String msrunName2 = "msrun2";
+        final String msrunName3 = "msrun3";
+        final String directory1 = TIC_DIRECTORY + "QE2_130603_OPL0000_mq_04_Arnold_04\\";
+        xySeries = readXYSeries(msrunName1, new File(directory1 + "QE2_130603_OPL0000_mq_04_Arnold_04_ticmatrix.csv"));
+        final String directory2 = TIC_DIRECTORY + "QE2_130603_OPL0000_mq_05_Arnold_05\\";
+        final File ticFileChartUnit3 = new File(directory2 + "QE2_130603_OPL0000_mq_05_Arnold_05_ticmatrix.csv");
+        // public ChartUnit(final String msrunName, final int reportNum, final XYSeries series)
+        chartUnit1 = new ChartUnit(msrunName1, 1, xySeries);
+        chartUnit2 = new ChartUnit(msrunName2, 2, null); //initialize with empty series
+        chartUnit3 = new ChartUnit(msrunName3, 3, readXYSeries(msrunName1, ticFileChartUnit3));
     }
 
     /**
-     * Test <code>getMaxTicIntensity</code> from <code>ChartUnit</code> class.
-     */
-    @SuppressWarnings("deprecation")
-	@Test
-    public void testgetMaxTicIntensity() {
-    	double maxIntensity1 = 22950831906.0;
-    	double maxIntensity2 = 12888332635.0;
-        System.out.println("maxIntensity1 = " + maxIntensity1 + " Obtained = " + chartUnit1.getMaxTicIntensity());
-        System.out.println("maxIntensity2 = " + maxIntensity2 + " Obtained = " + chartUnit3.getMaxTicIntensity());
-        assertEquals(0, chartUnit2.getMaxTicIntensity(), 0);
-        assertEquals(maxIntensity1, chartUnit1.getMaxTicIntensity(), 0);
-        assertEquals(maxIntensity2, chartUnit3.getMaxTicIntensity(), 0);
-    }
-
-    /**
-     * Test <code>setGraphDataSeries</code> from <code>ChartUnit</code> class.
+     * Test the <code>getMaxTicIntensity</code> method.
      */
     @Test
+    public void testGetMaxTicIntensity() {
+        double maxIntensity1 = 1.08153E9;
+        double maxIntensity2 = 0.0;
+        double maxIntensity3 = 1.43231E9;
+        assertEquals(maxIntensity1, chartUnit1.getMaxTicIntensity(), 0);
+        assertEquals(maxIntensity2, chartUnit2.getMaxTicIntensity(), 0);
+        assertEquals(maxIntensity3, chartUnit3.getMaxTicIntensity(), 0);
+    }
+
+    /**
+     * Test the <code>setGraphDataSeries</code> method.
+     * This test currently fails: we need to decide whether setGraphDataSeries should set maxIntensity.
+     */
+    @Test
+    @Ignore
     public void testSetGraphDataSeries() {
-    	double maxIntensity = 22950831906.0;
-        chartUnit2.setGraphDataSeries(series1);
+        double maxIntensity = 22950831906.0;
+        chartUnit2.setGraphDataSeries(xySeries);
+        // TODO: the assert below currently fails, because chartUnit2.getMaxTicIntensity() returns 0. [Freek]
+        // Should setGraphDataSeries set the maxIntensity (and maxIntensityString) fields in ChartUnit? [Freek]
+        // I think 1.08153E9 is the max tic intensity in xySeries, instead of 22950831906.0. [Freek]
         assertEquals(maxIntensity, chartUnit2.getMaxTicIntensity(), 0);
     }
 
     /**
-     * Test <code>getTicChart</code> and <code>getDomainAxis</code> from <code>ChartUnit</code> class.
+     * Test the <code>getTicChart</code> and <code>getDomainAxis</code> methods.
      */
     @Test
     public void testGetTicChartGetDomainAxis() {
@@ -85,40 +85,31 @@ public class ChartUnitTest {
         assertEquals("Expecting NumberAxis class.", NumberAxis.class, chartUnit1.getDomainAxis().getClass());
         assertEquals("Expecting NumberAxis class.", NumberAxis.class, chartUnit2.getDomainAxis().getClass());
     }
-    
+
     /**
-     * Create XYSeries by reading TIC matrix file that contains X & Y axis values representing TIC graph
-     * @param msrunName
-     * @param ticMatrixFile
-     * @return XYSeries
+     * Create an <code>XYSeries</code> object by reading the TIC matrix file that contains rt and ions values
+     * representing a TIC graph.
+     *
+     * @param msrunName the name of the msrun.
+     * @param ticMatrixFile the file with the TIC values.
+     * @return The <code>XYSeries</code> object with the TIC graph.
      */
     private XYSeries readXYSeries(final String msrunName, final File ticMatrixFile) {
-        BufferedReader br = null;
-    	try {
-    		br = new BufferedReader(new FileReader(ticMatrixFile));
-    	} catch (FileNotFoundException e) {
-    		e.printStackTrace();
-    	}
-        String line;
-        XYSeries series = new XYSeries(msrunName);
+        final XYSeries xySeries = new XYSeries(msrunName);
         try {
-            br.readLine(); //skip first line
-    		while ((line = br.readLine()) != null) {
-    		    StringTokenizer st = new StringTokenizer(line, ",");
-    		    // The first token is the x value.
-    		    String xValue = st.nextToken();
-    		    // The last token is the y value.
-    		    String yValue = st.nextToken();
-    		    float x = Float.parseFloat(xValue)/60;
-    		    float y = Float.parseFloat(yValue);
-    		    series.add(x, y);
-    		    //System.out.println("xValue = " + xValue + " x = " + x);
-    		}
-    	    br.close();
-    	} catch (NumberFormatException | IOException e) {
-    		e.printStackTrace();
-    	}
-        return series;
+            final BufferedReader bufferedReader = new BufferedReader(new FileReader(ticMatrixFile));
+            // Skip the header line ("rt","ions").
+            bufferedReader.readLine();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                final String[] values = line.split(",");
+                xySeries.add(Double.parseDouble(values[0]) / 60.0, Double.parseDouble(values[1]));
+            }
+            bufferedReader.close();
+        } catch (NumberFormatException | IOException e) {
+            fail(e.getMessage());
+            e.printStackTrace();
+        }
+        return xySeries;
     }
-    
 }
