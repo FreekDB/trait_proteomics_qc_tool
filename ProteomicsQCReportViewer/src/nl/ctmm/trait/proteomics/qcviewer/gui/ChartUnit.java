cb0c9e7bb4ba5e3f483302b5768caae11fc83487
Freek de Bruijn <freekdb@gmail.com>
2013-07-04 11:03:29 +0200

    Removed unused fields. Split the constructor by creating createBarRenderer. Simply always return maxIntensity from getMaxTicIntensity, because the field is initialized to zero. Small changes: put static fields at the top, made variables and parameters final if possible, changed fields into variables if possible, removed unnecessary null field initializations.

--- ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ChartUnit.java
+++ ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ChartUnit.java
@@ -17,93 +17,86 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  * The object of this class represents tic chart and corresponding plot, domainAxis, rangeAxis and 
- * renderer of single msreading.
+ * renderer of a single msreading.
  *
  * @author <a href="mailto:pravin.pawar@nbic.nl">Pravin Pawar</a>
  * @author <a href="mailto:freek.de.bruijn@nbic.nl">Freek de Bruijn</a>
  */
 public class ChartUnit {
-    private String chartName = "";
-    private XYSeries graphDataSeries = null;
-    private int reportNum; 
-    private JFreeChart ticChart = null; 
-    private NumberAxis domainAxis = null;
-    private NumberAxis rangeAxis = null;
-    private XYBarRenderer renderer = null;
-    private XYPlot plot = null;
-    private double maxIntensity;
-    private String maxIntensityString = "N/A";
     private static final List<Color> GRAPH_COLORS = Arrays.asList(
-           Color.BLUE, Color.DARK_GRAY, Color.GRAY, Color.MAGENTA, Color.ORANGE, 
-           Color.PINK, Color.LIGHT_GRAY, Color.RED, Color.GREEN);
-    
+            Color.BLUE, Color.DARK_GRAY, Color.GRAY, Color.MAGENTA, Color.ORANGE,
+            Color.PINK, Color.LIGHT_GRAY, Color.RED, Color.GREEN);
+
+    private XYSeries graphDataSeries;
+    private JFreeChart ticChart;
+    private NumberAxis domainAxis;
+    private double maxIntensity;
+
     public ChartUnit(final String msrunName, final int reportNum, final XYSeries series) {
-        this.chartName = msrunName; 
-        this.reportNum = reportNum;
         this.graphDataSeries = series;
-        
+        String maxIntensityString = "N/A";
         if (series != null) {
             maxIntensity = series.getMaxY();
             NumberFormat formatter = new DecimalFormat("0.0000E0");
             maxIntensityString = formatter.format(maxIntensity);
         }
         domainAxis = new NumberAxis(null);
-        rangeAxis = new NumberAxis(null);
-        renderer = new XYBarRenderer();
+        final XYBarRenderer renderer = createBarRenderer(reportNum);
+        final XYSeriesCollection xyDataset = new XYSeriesCollection(series);
+        //Prepare chart using plot - this is the best option to control domain and range axes
+        final NumberAxis rangeAxis = new NumberAxis(null);
+        final XYPlot plot = new XYPlot(xyDataset, domainAxis, rangeAxis, renderer);
+        rangeAxis.setNumberFormatOverride(new DecimalFormat("0E00"));
+        final Font font = new Font("Garamond", Font.BOLD, 13);
+        ticChart = new JFreeChart(msrunName + "     MaxIntensity = " + maxIntensityString, font, plot, false);
+        // performance
+        ticChart.setAntiAlias(false);
+    }
+
+    /**
+     * Create a <code>XYBarRenderer</code>.
+     * @param reportNum the report number, which is used for picking a color.
+     * @return the <code>XYBarRenderer</code>.
+     */
+    private XYBarRenderer createBarRenderer(final int reportNum) {
+        final XYBarRenderer renderer = new XYBarRenderer();
         //Sets the percentage amount by which the bars are trimmed
         renderer.setMargin(0.98); //Default renderer margin is 0.0
-        renderer.setDrawBarOutline(false); 
+        renderer.setDrawBarOutline(false);
         renderer.setShadowVisible(false);
-        Color currentColor = GRAPH_COLORS.get(reportNum%GRAPH_COLORS.size());
+        final Color currentColor = GRAPH_COLORS.get(reportNum % GRAPH_COLORS.size());
         renderer.setSeriesPaint(0, currentColor);
         renderer.setGradientPaintTransformer(null);
         renderer.setSeriesOutlinePaint(0, currentColor);
-        renderer.setFillPaint(currentColor);
-        renderer.setPaint(currentColor);
+
+        // TODO: check whether this is equivalent to calling setFillPaint and setPaint without series index. [Freek]
+        renderer.setBaseFillPaint(currentColor);
+        renderer.setBasePaint(currentColor);
+
         renderer.setBarPainter(new StandardXYBarPainter());
         renderer.setSeriesStroke(0, null);
         renderer.setBasePaint(Color.WHITE);
         XYBarRenderer.setDefaultShadowsVisible(false);
         //Add support for toolTip
-        StandardXYToolTipGenerator toolTipGenerator = new StandardXYToolTipGenerator();
+        final StandardXYToolTipGenerator toolTipGenerator = new StandardXYToolTipGenerator();
         renderer.setBaseToolTipGenerator(toolTipGenerator);
         renderer.setDrawBarOutline(false);
-        XYSeriesCollection xyDataset = new XYSeriesCollection(series);
-        //Prepare chart using plot - this is the best option to control domain and range axes
-        plot = new XYPlot(xyDataset, domainAxis, rangeAxis, renderer);
-        rangeAxis.setNumberFormatOverride(new DecimalFormat("0E00"));
-        int style = Font.BOLD;
-        Font font = new Font ("Garamond", style , 13);
-        ticChart = new JFreeChart(msrunName + "     MaxIntensity = " + maxIntensityString, font, plot, false);
-        // performance
-        ticChart.setAntiAlias(false);
+        return renderer;
     }
-    
+
     /**
      * Get max intensity of TIC graph
      * @return Max intensity of TIC graph
      */
     public double getMaxTicIntensity() {
-        // TODO: simply return maxIntensity, since it is 0 when graphDataSeries is null? [Freek]
-        if (graphDataSeries == null) {
-            return 0;
-        } else
-            return maxIntensity;
+        return maxIntensity;
     }
 
     /**
-     * Get max intensity of TIC graph in String format
-     * @return Max intensity of TIC graph in String format
+     * Set the value of parameter graphDataSeries.
+     * @param series the series to use.
      */
-    public String getMaxTicIntensityString() {
-        return maxIntensityString;
-    }
-    
-    /**
-     * Set the value of parameter graphDataSeries
-     * @param series
-     */
-    public void setGraphDataSeries(XYSeries series) {
+    public void setGraphDataSeries(final XYSeries series) {
         this.graphDataSeries = series;
     }
     
