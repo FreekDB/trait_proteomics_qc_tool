ce990aecc2f219cada6ee39ed5550b7cbfbcf896
Freek de Bruijn <freekdb@gmail.com>
2013-07-28 01:39:04 +0200

    Used logger instead of printStackTrace. Added Javadocs, changed some comments and renamed some parameters & variables.

--- ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/input/MetricsParser.java
+++ ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/input/MetricsParser.java
@@ -8,31 +8,52 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import nl.ctmm.trait.proteomics.qcviewer.gui.SortedListModel;
 import nl.ctmm.trait.proteomics.qcviewer.utils.Constants;
 
 /**
- * The class for parsing metrics IDs and names from MetricsListing.txt file
+ * The class for parsing metrics IDs and descriptions from the MetricsListing.txt file.
  *
  * @author <a href="mailto:pravin.pawar@nbic.nl">Pravin Pawar</a>
  * @author <a href="mailto:freek.de.bruijn@nbic.nl">Freek de Bruijn</a>
  */
 public class MetricsParser {
-    private Properties appProperties;
-    private Map<String, String> allMetricsMap;
-    
     /**
-     * Constructor
-     * @param appProperties Application properties
+     * The logger for this class.
+     */
+    private static final Logger logger = Logger.getLogger(MetricsParser.class.getName());
+
+    /**
+     * The application properties.
+     * <p/>
+     * TODO: maybe it's better to put the responsibility for writing properties in a separate Settings class? [Freek]
+     */
+    private Properties appProperties;
+
+    /**
+     * The map with all metrics supported by the NIST QC pipeline. The keys are "category:code" strings and the values
+     * are descriptions of the metrics.
+     * <p/>
+     * For example, key: "dyn:ds-1a" and value: "Ratios of Peptide Ions IDed (Once/Twice)".
+     */
+    private Map<String, String> allMetricsMap;
+
+    /**
+     * Construct a metrics parser.
+     *
+     * @param appProperties the application properties.
      */
     public MetricsParser(final Properties appProperties) {
         this.appProperties = appProperties;
     }
-    
+
     /**
-     * Get all the metrics read from MetricsListing.txt file
-     * @return map containing key and value of metrics
-     * e.g. Key: "dyn:ds-1a" Value: "Ratios of Peptide Ions IDed (Once/Twice)"
+     * Get all the metrics by reading them from the MetricsListing.txt file.
+     *
+     * @return map containing key and value of metrics.
      */
     public Map<String, String> getMetricsListing() {
         if (allMetricsMap == null) {
@@ -41,46 +62,50 @@ public class MetricsParser {
         return allMetricsMap;
     }
 
-    /*
-     * Reads all the metrics from MetricsListing.txt file
+    /**
+     * Read all the metrics from the MetricsListing.txt file.
      */
     private void readMetricsListing() {
-        // Create a HashMap which stores Strings as the keys and values
         allMetricsMap = new HashMap<>();
         try {
             final BufferedReader reader = new BufferedReader(new FileReader(Constants.PROPERTY_METRICS_LISTING_FILE));
             String line;
             while ((line = reader.readLine()) != null) {
-                final StringTokenizer lineTokenizer = new StringTokenizer(line, ":");
+                final String separator = ":";
+                final StringTokenizer lineTokenizer = new StringTokenizer(line, separator);
                 // The first two tokens together form key of the metric e.g. "ion:is-1b".
-                final String key = lineTokenizer.nextToken() + ":" + lineTokenizer.nextToken();
-                // The second token is name of the metric.
+                final String key = lineTokenizer.nextToken() + separator + lineTokenizer.nextToken();
+                // The second token is description of the metric.
                 final String value = lineTokenizer.nextToken();
                 allMetricsMap.put(key, value);
             }
             reader.close();
         } catch (final NumberFormatException | IOException e) {
-            e.printStackTrace();
+            final String message = "Something went wrong while reading the metrics definition file: %s.";
+            logger.log(Level.SEVERE, String.format(message, Constants.PROPERTY_METRICS_LISTING_FILE), e);
         }
     }
 
     /**
-     * Update metrics in the report frame
-     * Update TopColumnNamesV2 property from appProperties file 
-     * @param move A sorted list containing names of selected metrics
+     * Update metrics in the report frame and update the TopColumnNamesV2 property in the application properties file.
+     *
+     * TODO: looks like updating the GUI is done elsewhere. If that's the case, we can rename this method. [Freek]
+     *
+     * @param selectedMetrics a sorted list containing the names of the selected metrics.
      */
-    public void updateMetricsToDisplay(final SortedListModel move) {
-        String selectedMetrics = "";
-        for (int metricIndex = 0; metricIndex < move.getSize(); ++metricIndex) {
-            selectedMetrics = selectedMetrics.concat(move.getElementAt(metricIndex) + ",");
+    public void updateMetricsToDisplay(final SortedListModel selectedMetrics) {
+        String selectedMetricsString = "";
+        for (int metricIndex = 0; metricIndex < selectedMetrics.getSize(); metricIndex++) {
+            selectedMetricsString += selectedMetrics.getElementAt(metricIndex) + ",";
         }
-        appProperties.setProperty(Constants.PROPERTY_TOP_COLUMN_NAMESV2, selectedMetrics);
+        appProperties.setProperty(Constants.PROPERTY_TOP_COLUMN_NAMESV2, selectedMetricsString);
         try {
             final FileOutputStream outputStream = new FileOutputStream(Constants.PROPERTIES_FILE_NAME);
             appProperties.store(outputStream, null);
             outputStream.close();
         } catch (final IOException e) {
-            e.printStackTrace();
+            final String message = "Something went wrong while writing the application properties file: %s.";
+            logger.log(Level.SEVERE, String.format(message, Constants.PROPERTIES_FILE_NAME), e);
         }
     }
 }
