8d394b7d926dbd2b233d910b9c95f8fce1ff606c
ppawar <pravin.pawar@nbic.nl>
2013-08-19 14:01:46 +0200

    Removed unused import from ViewerFrame.java.

--- ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ViewerFrame.java
+++ ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ViewerFrame.java
@@ -32,7 +32,6 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
