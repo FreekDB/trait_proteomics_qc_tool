9b19ffedc78ccdf42cf75a810f3a2df03eecb474
ppawar <pravin.pawar@nbic.nl>
2013-08-20 16:59:54 +0200

    ReportUnit.java: Removed getComparator method. Main.java and ViewerFrame.java now use ReportUnit.getComparatorv2(...) for sorting mechanism.

--- ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ViewerFrame.java
+++ ProteomicsQCReportViewer/src/nl/ctmm/trait/proteomics/qcviewer/gui/ViewerFrame.java
@@ -1361,7 +1361,7 @@ public class ViewerFrame extends JFrame implements ActionListener, ItemListener,
             //Copy reportUnits to orderedReportUnits
             orderedReportUnits.addAll(reportUnits);
             //Sort orderedReportUnits according to sortKey and sort order - ascending/descending
-            Collections.sort(orderedReportUnits, ReportUnit.getComparator(sortKey, ascending));
+            Collections.sort(orderedReportUnits, ReportUnit.getComparatorV2(sortKey, ascending));
             //Create chart frames and add them to the desktop pane.
             prepareChartsInOrder(true);
             // Set first report graph in the Tic Pane. 
